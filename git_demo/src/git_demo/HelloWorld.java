2a1004a74a76ce55f7668b1f1c5933dc5d489bc3
kameramann <kameramann0823@gmail.com>
2015-10-19 09:23:03 +0800

    公司提交测试

--- git_demo/src/git_demo/HelloWorld.java
+++ git_demo/src/git_demo/HelloWorld.java
@@ -3,5 +3,6 @@ package git_demo;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Lzs say:hello world!hahala!");
+		System.out.println("Please lzs...");
 	}
 }
